1bfa89fab1d0df928534fe2bb55c73015580e85a
Ivan Hawkes <ivan.hawkes@gmail.com>
2021-08-02 20:32:40 +1000

    Added new pattern test.

---
+++ 02-pattern-test/02-pattern-test.asm
@@ -0,0 +1,54 @@
+	; To compile:
+	;    vasm6502_oldstyle.exe 02-pattern-test.asm -c02 -dotdir -Fbin
+
+	.org $8000
+
+PORTA = $6001
+PORTB = $6000
+DDRA = $6003
+DDRB = $6002
+
+PATTERNA = %00000000
+PATTERNB = %11111111
+PATTERNC = %00001111
+PATTERND = %11110000
+PATTERNE = %10101010
+PATTERNF = %01010101
+
+reset:
+	lda #%11111111			; Set all pins on port B to output.
+	sta DDRB
+
+	lda #%10101010			; Place a pattern in the accumulator.
+	sta PORTB				; Write the pattern to port B.
+
+loop:
+	lda #PATTERNA
+	sta PORTB				; Write the pattern to port B.
+	nop
+
+	lda #PATTERNB
+	sta PORTB				; Write the pattern to port B.
+	nop
+
+	lda #PATTERNC
+	sta PORTB				; Write the pattern to port B.
+	nop
+
+	lda #PATTERND
+	sta PORTB				; Write the pattern to port B.
+	nop
+
+	lda #PATTERNE
+	sta PORTB				; Write the pattern to port B.
+	nop
+
+	lda #PATTERNF
+	sta PORTB				; Write the pattern to port B.
+	nop
+
+	jmp loop
+
+	.org #$fffc				; 6502 bootup reads this address and jumps to it.
+	.word reset
+	.word $0000
---
+++ 03-rotate-right/03-rotate-right.asm
@@ -0,0 +1,25 @@
+	; To compile:
+	;    vasm6502_oldstyle.exe 03-rotate-right.asm -c02 -dotdir -Fbin
+
+	.org $8000
+
+PORTA = $6001
+PORTB = $6000
+DDRA = $6003
+DDRB = $6002
+
+reset:
+	lda #%11111111			; Set all pins on port B to output.
+	sta DDRB
+
+	lda #%10101010			; Place a pattern in the accumulator.
+	sta PORTB				; Write the pattern to port B.
+
+loop:
+	ror						; Rotate the pattern of bits.
+	sta PORTB				; Write the pattern to port B.
+	jmp loop
+
+	.org #$fffc				; 6502 bootup reads this address and jumps to it.
+	.word reset
+	.word $0000
